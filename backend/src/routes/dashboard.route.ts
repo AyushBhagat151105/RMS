c9c51e571eb43e1d4b1e205be9df6e18bbb3ba19
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-06-13 23:38:52 +0530

    feat: enhance dashboard API with new endpoints and authentication
    - Added new endpoints for retrieving total orders, total orders by status, total waiters, and total kitchens for a specific restaurant.
    - Implemented authentication middleware for the dashboard routes to secure access to the new endpoints.
    - Updated Swagger documentation to reflect the new server configuration and API changes.

--- backend/src/routes/dashboard.route.ts
+++ backend/src/routes/dashboard.route.ts
@@ -4,14 +4,28 @@ import {
   getTotalOrdersByStatus,
   getTotalWaiter,
 } from "@/controllers/dashboard.controller";
+import { isAuthenticated } from "@/middlewares/isAuthenticated";
 import { Router } from "express";
 
 export const dashboardRoute = Router();
 
-dashboardRoute.get("/total-orders/:restaurantId", getTotalOrders);
+dashboardRoute.get(
+  "/total-orders/:restaurantId",
+  isAuthenticated,
+  getTotalOrders
+);
 dashboardRoute.get(
   "/total-orders-by-status/:restaurantId",
+  isAuthenticated,
   getTotalOrdersByStatus
 );
-dashboardRoute.get("/total-waiter/:restaurantId", getTotalWaiter);
-dashboardRoute.get("/total-kitchen/:restaurantId", getTotalKitchen);
+dashboardRoute.get(
+  "/total-waiter/:restaurantId",
+  isAuthenticated,
+  getTotalWaiter
+);
+dashboardRoute.get(
+  "/total-kitchen/:restaurantId",
+  isAuthenticated,
+  getTotalKitchen
+);
